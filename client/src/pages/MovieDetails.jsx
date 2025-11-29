1c772937f7c0f428c109f423ed85125ba66128fe
sujan2000 <meena.sujan14@gmail.com>
2025-11-30 01:56:20 +0530

    casts  img and name added

--- client/src/pages/MovieDetails.jsx
+++ client/src/pages/MovieDetails.jsx
@@ -44,21 +44,38 @@ const MovieDetails = () => {
             {timeFormat(show.movie.runtime)} • {show.movie.genres.map(genre =>
               genre.name).join(", ")} • {show.movie.release_date.split("-")[0]}
           </p>
+
+
           <div className="flex items-center flex-wrap gap-4 mt-4">
             <button className="flex items-center gap-2 px-7 py-3 text-sm bg-gray-800
             hover:bg-gray-900 transition rounded-md font-medium cursor-pointer active:scale-95">
               <PlayCircle className="w-5 h-5" />
               Watch Trailer
             </button>
-            <a href="" className="px-10 py-3 text-sm bg-primary hover:bg-primary-dull
+            <a href="#dateSelect" className="px-10 py-3 text-sm bg-primary hover:bg-primary-dull
              transition rounded-md font-medium cursor-pointer active:scale-95">Buy Tickets</a>
             <button className="bg-gray-700 p-2.5 rounded-full transition 
             cursor-pointer active:scale-95">
               <Heart className={`w-5 h-5`} />
             </button>
           </div>
+
         </div>
       </div>
+
+      <p className="text-lg font-medium mt-20">Your Favorite Cast</p>
+      <div className="overflow-x-auto no-scrollbar mt-8 pb-4">
+        <div className="flex items-center gap-4 w-max px-4">
+          {show.movie.casts.slice(0,12).map((cast,index) => (
+            <div kay={index} className="flex flex-col items-center text-center">
+              <img src={cast.profile_path} alt="" 
+              className="rounded-full h-20 md:h-20 aspect-square object-cover" />
+              <p className="font-medium text-xs mt-3">{cast.name}</p>
+            </div>
+          ))}
+        </div>
+      </div>
+
     </div>
   ) : (
     <div>
