d51cd3c35af0823ac6618d92d0fd2eccf8c3eeba
sujan2000 <meena.sujan14@gmail.com>
2025-11-30 01:28:57 +0530

    time date overview added

--- client/src/pages/MovieDetails.jsx
+++ client/src/pages/MovieDetails.jsx
@@ -3,6 +3,7 @@ import { useParams } from 'react-router-dom'
 import { dummyDateTimeData, dummyShowsData } from '../assets/assets'
 import BlurCircle from '../components/BlurCircle'
 import { StarIcon } from 'lucide-react'
+import timeFormat from '../lib/timeFormat'
 
 const MovieDetails = () => {
 
@@ -23,20 +24,28 @@ const MovieDetails = () => {
 
   return show ? (
     <div className="px-6 md:px-16 lg:px pt-30 md:pt-50">
-        <div className="flex flex-col md:flex-row gap-8 max-w-6xl mx-auto">
-             <img src={show.movie.poster_path} alt="" className="max-md:mx-auto rounded-xl h-104
-              max-w-70 object-cover"/> 
+      <div className="flex flex-col md:flex-row gap-8 max-w-6xl mx-auto">
+        <img src={show.movie.poster_path} alt="" className="max-md:mx-auto rounded-xl h-104
+              max-w-70 object-cover"/>
 
-              <div className="relative flex flex-col gap-3">
-               <BlurCircle top="-100px"  left="-100px"/>
-               <p className="text-primary">ENGLISH</p>
-               <h1 className="text-4xl font-semibold max-w-96 text-balance">{show.movie.title}</h1>
-               <div className="flex items-center gap-2 text-gray-300">
-                   <StarIcon className="w-5 h-5 text-primary fill-primary"/>
-                   {show.movie.vote_average.toFixed(1)} User Rating
-               </div>
-              </div>
+        <div className="relative flex flex-col gap-3">
+          <BlurCircle top="-100px" left="-100px" />
+          <p className="text-primary">ENGLISH</p>
+          <h1 className="text-4xl font-semibold max-w-96 text-balance">
+            {show.movie.title}
+          </h1>
+          <div className="flex items-center gap-2 text-gray-300">
+            <StarIcon className="w-5 h-5 text-primary fill-primary" />
+            {show.movie.vote_average.toFixed(1)} User Rating
+          </div>
+          <p className="text-gray-400 mt-2 text-sm
+           leading-tight max-w-xl">{show.movie.overview}</p>
+          <p>
+            {timeFormat(show.movie.runtime)} • {show.movie.genres.map(genre =>
+              genre.name).join(", ")} • {show.movie.release_date.split("-")[0]}
+          </p>
         </div>
+      </div>
     </div>
   ) : (
     <div>
