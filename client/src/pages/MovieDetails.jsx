4ee3707d87f751c9529d752b8667082846585250
sujan2000 <meena.sujan14@gmail.com>
2025-11-30 01:04:59 +0530

    condition added

--- client/src/pages/MovieDetails.jsx
+++ client/src/pages/MovieDetails.jsx
@@ -1,23 +1,31 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import { useParams } from 'react-router-dom'
 import { dummyDateTimeData, dummyShowsData } from '../assets/assets'
 
 const MovieDetails = () => {
-  
-  const {id} = useParams()
+
+  const { id } = useParams()
   const [show, setShow] = useState(null)
 
-  const getShow =async ()=>{
-    const show =dummyShowsData.find(show => show._id === id)
+  const getShow = async () => {
+    const show = dummyShowsData.find(show => show._id === id)
     setShow({
       movie: show,
       dateTime: dummyDateTimeData
     })
   }
 
-  return (
-    <div>
+  useEffect(()=>{
+     getShow()
+  },[id])
 
+  return show ?  (
+    <div>
+       
+    </div>
+  ) :(
+    <div>
+      Loading...
     </div>
   )
 }
