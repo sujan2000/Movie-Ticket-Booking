3c5b7aa86ee67d698b06c844e4b37f5ddf838a5c
sujan2000 <meena.sujan14@gmail.com>
2025-11-30 00:31:07 +0530

    dummy shows data working  now

--- client/src/pages/Movies.jsx
+++ client/src/pages/Movies.jsx
@@ -1,8 +1,20 @@
 import React from 'react'
+import { dummyShowsData } from '../assets/assets'
+import MovieCard from '../components/MovieCard'
+
 
 const Movies = () => {
-  return (
-    <div>Movies</div>
+  return dummyShowsData.length > 0 ? (
+    <div>
+      <h1>Now Showing</h1>
+      <div>
+        {dummyShowsData.map((movie) => (
+          <MovieCard movie={movie} key={movie._id} />
+        ))}
+      </div>
+    </div>
+  ) : (
+    <div>No movies available</div>
   )
 }
 
