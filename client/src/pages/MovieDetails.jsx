8580d3f2ca7d2440120b4d4018f933d95eb52ffd
sujan2000 <meena.sujan14@gmail.com>
2025-11-30 01:02:38 +0530

    data and details fateched

--- client/src/pages/MovieDetails.jsx
+++ client/src/pages/MovieDetails.jsx
@@ -1,8 +1,24 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { useParams } from 'react-router-dom'
+import { dummyDateTimeData, dummyShowsData } from '../assets/assets'
 
 const MovieDetails = () => {
+  
+  const {id} = useParams()
+  const [show, setShow] = useState(null)
+
+  const getShow =async ()=>{
+    const show =dummyShowsData.find(show => show._id === id)
+    setShow({
+      movie: show,
+      dateTime: dummyDateTimeData
+    })
+  }
+
   return (
-    <div>MovieDetails</div>
+    <div>
+
+    </div>
   )
 }
 
