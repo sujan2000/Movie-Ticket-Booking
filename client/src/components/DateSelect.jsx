18c60cea2f606e831a9c7b1544a4c29cc20ee72a
sujan2000 <meena.sujan14@gmail.com>
2025-12-01 00:00:44 +0530

    date time of show add

--- client/src/components/DateSelect.jsx
+++ client/src/components/DateSelect.jsx
@@ -1,8 +1,8 @@
 import React from 'react'
 import BlurCircle from './BlurCircle'
-import { ChevronLastIcon } from 'lucide-react'
+import { ChevronLastIcon, ChevronRightIcon } from 'lucide-react'
 
-const DateSelect = ({dateTiem, id}) => {
+const DateSelect = ({dateTime, id}) => {
   return (
     <div id="dateSelect" className="pt-30">
       <div className='flex flex-col md:flex-row items-center justify-between gap-10 
@@ -13,8 +13,20 @@ const DateSelect = ({dateTiem, id}) => {
         <p className="text-lg font-semibold">Choose Date</p>
         <div className="flex items-center gap-6 text-sm mt-5">
           <ChevronLastIcon width={28}/>
+          <span className="grid gird-cols-3 md:flex flex-wrap md:max-w-lg gap-4">
+            {Object.keys(dateTime).map((date)=>(
+                <button key={date} className="flex flex-col items-center justify-center h-14 w-14 aspect-square
+                rounded cursor-pointer">
+                  <span>{new Date(date).getDate()}</span>
+                  <span>{new Date(date).toLocaleDateString("en-US", {month:"short"})}</span>
+                </button>
+            ))}
+          </span>
+          <ChevronRightIcon width={28}/>
         </div>
        </div>
+   
+       <button className="">Book Now</button>
       </div>
     </div>
   )
