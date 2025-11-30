7839c6c5fe6e1f3e1d4695c452b0844ed72ab8a9
sujan2000 <meena.sujan14@gmail.com>
2025-12-01 00:53:55 +0530

    seat Layout id date get

--- client/src/pages/SeatLayout.jsx
+++ client/src/pages/SeatLayout.jsx
@@ -1,8 +1,13 @@
 import React from 'react'
+import { useParams } from 'react-router-dom'
 
 const SeatLayout = () => {
+
+   const [id, date] = useParams()
   return (
-    <div>SeatLayout</div>
+    <div>
+
+    </div>
   )
 }
 
