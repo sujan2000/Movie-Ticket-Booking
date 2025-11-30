bd459d91757875b481514288bbb5efca72451f8d
sujan2000 <meena.sujan14@gmail.com>
2025-12-01 01:34:36 +0530

    choose time section complete

--- client/src/pages/SeatLayout.jsx
+++ client/src/pages/SeatLayout.jsx
@@ -3,6 +3,7 @@ import { useNavigate, useParams } from 'react-router-dom'
 import { dummyDateTimeData, dummyShowsData } from '../assets/assets'
 import Loading from '../components/Loading'
 import { ClockIcon } from 'lucide-react'
+import isoTimeFormat from '../lib/isoTimeFormat'
 
 const SeatLayout = () => {
 
@@ -33,12 +34,12 @@ const SeatLayout = () => {
       <div className="w-60 bg-primary/10 border border-primary/20 rounded-lg py-10 
        h-max md:sticky md:top-30">
         <p className="text-lg font-semibold px-6">Available Timings</p>
-        <div>
-          {show.dateTime[date].map((item)=>(
-            <div className={`flex items-center gap-2 px-6 py-2 w-max rounded-r-md
+        <div className="mt-5 space-y-1">
+          {show.dateTime[date].map((item) => (
+            <div key={item.time} onClick={()=>setSelectedTime(item)} className={`flex items-center gap-2 px-6 py-2 w-max rounded-r-md
              cursor-pointer transition ${selectedTime?.time === item.time ? "bg-primary text-white" : "hover:bg-primary/20"}`}>
-              <ClockIcon className="w-4 h-4"/>
-              <p className="text-sm">{item.time}</p>
+              <ClockIcon className="w-4 h-4" />
+              <p className="text-sm">{isoTimeFormat(item.time)}</p>
             </div>
           ))}
         </div>
