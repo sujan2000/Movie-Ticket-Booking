0307d67d4e6acfb26aaa2bdc7ffef6a54e4696c6
sujan2000 <meena.sujan14@gmail.com>
2025-12-01 01:08:22 +0530

    seatlayout and available show add

--- client/src/pages/SeatLayout.jsx
+++ client/src/pages/SeatLayout.jsx
@@ -1,13 +1,45 @@
-import React from 'react'
-import { useParams } from 'react-router-dom'
+import React, { useEffect, useState } from 'react'
+import { useNavigate, useParams } from 'react-router-dom'
+import { dummyDateTimeData, dummyShowsData } from '../assets/assets'
+import Loading from '../components/Loading'
 
 const SeatLayout = () => {
 
-   const [id, date] = useParams()
-  return (
-    <div>
+  const { id, date } = useParams()
+  const [selectedSeats, setSelectedSeats] = useState([])
+  const [selectedTime, setSelectedTime] = useState(null)
+  const [show, setShow] = useState(null)
 
+  const navigate = useNavigate()
+
+  const getShow = async () => {
+    const show = dummyShowsData.find(show => show._id === id)
+    if (show) {
+      setShow({
+        movie: show,
+        dateTime: dummyDateTimeData
+      })
+    }
+  }
+
+  useEffect(() => {
+    getShow()
+  }, [])
+
+  return show ? (
+    <div className="flex flex-col md:flex-row px-6 md:px-16 lg:px-40 py-30 md:pt-50">
+      {/* {} */}
+      <div className="w-60 bg-primary/10 border border-primary/20 rounded-lg py-10 
+       h-max md:sticky md:top-30">
+        <p className="text-lg font-semibold px-6">Available Timings</p>
+      </div>
+
+      {/*  */}
+
+      <div></div>
     </div>
+  ) : (
+    <Loading />
   )
 }
 
