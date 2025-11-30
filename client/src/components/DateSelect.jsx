b80e059fb0d99d490c75ab2b6f7a432699b7ec93
sujan2000 <meena.sujan14@gmail.com>
2025-11-30 23:42:00 +0530

    blur circle and style classes added

--- client/src/components/DateSelect.jsx
+++ client/src/components/DateSelect.jsx
@@ -1,9 +1,13 @@
 import React from 'react'
+import BlurCircle from './BlurCircle'
 
 const DateSelect = ({dateTiem, id}) => {
   return (
-    <div id="">
-
+    <div id="dateSelect" className="pt-30">
+      <div className='flex flex-col md:flex-row items-center justify-between gap-10 
+      relative p-8 bg-primary/10 border border-primary/20 rounded-lg'>
+       <BlurCircle />
+      </div>
     </div>
   )
 }
