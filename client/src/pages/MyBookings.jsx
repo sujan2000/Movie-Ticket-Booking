3c1eeb970a416ec24ead9918bd0a09794d9583a4
sujan2000 <meena.sujan14@gmail.com>
2025-12-02 01:06:24 +0530

    seat and ticket number show

--- client/src/pages/MyBookings.jsx
+++ client/src/pages/MyBookings.jsx
@@ -50,6 +50,12 @@ const MyBookings = () => {
               {!item.isPaid && <button className="bg-primary px-4 py-1.5 mb-3 text-sm rounded-full 
               font-medium cursor-pointer">Pay Now</button>}
             </div>
+
+            <div className="text-sm">
+              <p><span className="text-gray-400">Total Tickets:</span>{item.bookings.length}</p>
+              <p><span className="text-gray-400">Seat Number:</span>{item.bookedSeat.join(', ')}</p>
+            </div>
+
           </div>
 
         </div>
