b58e0c73e32ea6494e2d23d6ffc6255400be2753
sujan2000 <meena.sujan14@gmail.com>
2025-12-02 00:30:29 +0530

    blur circles added

--- client/src/pages/MyBookings.jsx
+++ client/src/pages/MyBookings.jsx
@@ -1,25 +1,34 @@
 import React, { useEffect, useState } from 'react'
 import { dummyBookingData } from '../assets/assets'
+import Loading from '../components/Loading'
+import BlurCircle from '../components/BlurCircle'
 
 const MyBookings = () => {
 
-   const currency = import.meta.env.VITE_CURRENCY
-   const [bookings, setBookings] = useState([])
-   const [isLoading, setIsLoading] = useState(true)
+  const currency = import.meta.env.VITE_CURRENCY
+  const [bookings, setBookings] = useState([])
+  const [isLoading, setIsLoading] = useState(true)
 
-   const getMyBookings = async()=>{
+  const getMyBookings = async () => {
     setBookings(dummyBookingData)
     setIsLoading(false)
-   }
+  }
 
-   useEffect(()=>{
+  useEffect(() => {
     getMyBookings()
-   },[])
+  }, [])
 
-  return (
-    <div>
-         
+  return !isLoading ? (
+    <div className="relative px-6 md:px-16 lg:px-40 pt-30 md:pt-40 min-h-[80vh]">
+       <BlurCircle top="100px" left="100px"/>
+       <div>
+        <BlurCircle bottom="0px" left="600px"/>
+       </div>
+       <h1 className="text-lg font-semibold mb-4">My Bookings</h1>
+        
     </div>
+  ) : (
+    <Loading />
   )
 }
 
