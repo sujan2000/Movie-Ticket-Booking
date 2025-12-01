821469b7a0c77556742061c0cd9acea20a309279
sujan2000 <meena.sujan14@gmail.com>
2025-12-02 00:38:47 +0530

    booking card added

--- client/src/pages/MyBookings.jsx
+++ client/src/pages/MyBookings.jsx
@@ -25,7 +25,20 @@ const MyBookings = () => {
         <BlurCircle bottom="0px" left="600px"/>
        </div>
        <h1 className="text-lg font-semibold mb-4">My Bookings</h1>
-        
+
+        {bookings.map((item,index)=>(
+          <div key={index} className="flex flex-col md:flex-row justify-between
+          bg-primary/8 border border-primary/20 rounded-lg mt-4 p-2 max-w-3xl">
+             <div className="flex flex-col md:flex-row">
+              <img src={item.show.movie.poster_path} alt="" className="md:max-w-45 *:
+               aspect-video h-auto object-cover object-bottom rounded"/>
+               <div className="flex flex-col p-4">
+                <p>{item.show.movie.title}</p>
+               </div>
+             </div>
+          </div>
+        ))}
+
     </div>
   ) : (
     <Loading />
