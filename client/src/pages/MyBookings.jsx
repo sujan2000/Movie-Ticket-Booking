7c3143944591873626911a60fbaa72113c6d3195
sujan2000 <meena.sujan14@gmail.com>
2025-12-02 00:51:14 +0530

    dateFormat use dor date

--- client/src/pages/MyBookings.jsx
+++ client/src/pages/MyBookings.jsx
@@ -2,6 +2,8 @@ import React, { useEffect, useState } from 'react'
 import { dummyBookingData } from '../assets/assets'
 import Loading from '../components/Loading'
 import BlurCircle from '../components/BlurCircle'
+import timeFormat from '../lib/timeFormat'
+import { dateFormat } from '../lib/dateFormat'
 
 const MyBookings = () => {
 
@@ -20,26 +22,26 @@ const MyBookings = () => {
 
   return !isLoading ? (
     <div className="relative px-6 md:px-16 lg:px-40 pt-30 md:pt-40 min-h-[80vh]">
-       <BlurCircle top="100px" left="100px"/>
-       <div>
-        <BlurCircle bottom="0px" left="600px"/>
-       </div>
-       <h1 className="text-lg font-semibold mb-4">My Bookings</h1>
+      <BlurCircle top="100px" left="100px" />
+      <div>
+        <BlurCircle bottom="0px" left="600px" />
+      </div>
+      <h1 className="text-lg font-semibold mb-4">My Bookings</h1>
 
-        {bookings.map((item,index)=>(
-          <div key={index} className="flex flex-col md:flex-row justify-between
+      {bookings.map((item, index) => (
+        <div key={index} className="flex flex-col md:flex-row justify-between
           bg-primary/8 border border-primary/20 rounded-lg mt-4 p-2 max-w-3xl">
-             <div className="flex flex-col md:flex-row">
-              <img src={item.show.movie.poster_path} alt="" className="md:max-w-45 *:
+          <div className="flex flex-col md:flex-row">
+            <img src={item.show.movie.poster_path} alt="" className="md:max-w-45 *:
                aspect-video h-auto object-cover object-bottom rounded"/>
-               <div className="flex flex-col p-4">
-                <p className="text-lg font-semibold">{item.show.movie.title}</p>
-                <p className="text-gray-400 text-sm">{item.show.movie.runtime}</p>
-                <p className="text-gray-400 text-sm mt-auto">{item.show.showDateTime}</p>
-               </div>
-             </div>
+            <div className="flex flex-col p-4">
+              <p className="text-lg font-semibold">{item.show.movie.title}</p>
+              <p className="text-gray-400 text-sm">{timeFormat(item.show.movie.runtime)}</p>
+              <p className="text-gray-400 text-sm mt-auto">{dateFormat(item.show.showDateTime)}</p>
+            </div>
           </div>
-        ))}
+        </div>
+      ))}
 
     </div>
   ) : (
