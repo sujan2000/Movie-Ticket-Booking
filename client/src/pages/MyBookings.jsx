aaaf5fa0e657b273c073f076907dc8c4067499f8
sujan2000 <meena.sujan14@gmail.com>
2025-12-02 00:20:46 +0530

    get bookings added

--- client/src/pages/MyBookings.jsx
+++ client/src/pages/MyBookings.jsx
@@ -1,8 +1,25 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { dummyBookingData } from '../assets/assets'
 
 const MyBookings = () => {
+
+   const currency = import.meta.env.VITE_CURRENCY
+   const [bookings, setBookings] = useState([])
+   const [isLoading, setIsLoading] = useState(true)
+
+   const getMyBookings = async()=>{
+    setBookings(dummyBookingData)
+    setIsLoading(false)
+   }
+
+   useEffect(()=>{
+    getMyBookings()
+   },[])
+
   return (
-    <div>MyBookings</div>
+    <div>
+         
+    </div>
   )
 }
 
