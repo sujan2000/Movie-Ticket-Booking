3fb3391def5a44fcea342245bc62c80c75ed7f67
sujan2000 <meena.sujan14@gmail.com>
2025-12-03 01:20:46 +0530

    list shows data get

--- client/src/pages/admin/ListShows.jsx
+++ client/src/pages/admin/ListShows.jsx
@@ -1,8 +1,37 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { dummyShowsData } from '../../assets/assets'
 
 const ListShows = () => {
+
+  const currency = import.meta.env.VITE_CURRENCY
+
+  const [shows, setShows] = useState([])
+  const [loading, setLoading] = useState(true)
+
+  const getAllShows = async () => {
+    try {
+
+      setShows([{
+        movie: dummyShowsData[0],
+        showDateTime: "2025-11-30T02:30:00.000Z",
+        showPrice: 59,
+        occupiedSeats: {
+          A1: "user_1",
+          B1: "user_2",
+          C1: "user_3"
+        }
+      }]);
+      setLoading(false)
+
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
   return (
-    <div>ListShows</div>
+    <div>
+
+    </div>
   )
 }
 
