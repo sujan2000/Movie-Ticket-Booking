91d307edccfa564d36e68264d6ab1add2a412f22
sujan2000 <meena.sujan14@gmail.com>
2025-12-03 01:36:10 +0530

    list shows path update

--- client/src/components/admin/AdminSidebar.jsx
+++ client/src/components/admin/AdminSidebar.jsx
@@ -15,7 +15,7 @@ const AdminSidebar = () => {
     const adminNavlinks = [
         { name: 'Dashboard', path: '/admin', icon: LayoutDashboardIcon },
         { name: 'Add Shows', path: '/admin/add-shows', icon: PlusSquareIcon },
-        { name: 'List Shows', path: '/admin/list-show', icon: ListIcon },
+        { name: 'List Shows', path: '/admin/list-shows', icon: ListIcon },
         { name: 'List Bookings', path: '/admin/list-bookings', icon: ListCollapseIcon },
     ]
 
