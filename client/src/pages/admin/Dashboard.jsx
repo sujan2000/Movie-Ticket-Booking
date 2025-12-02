03edb718d16bceb9388245bb7f626f368e706036
sujan2000 <meena.sujan14@gmail.com>
2025-12-03 00:52:14 +0530

    movie cards added

--- client/src/pages/admin/Dashboard.jsx
+++ client/src/pages/admin/Dashboard.jsx
@@ -20,7 +20,7 @@ const Dashboard = () => {
 
   const dashboardCards = [
     { title: "Total Bookings", value: dashboardData.totalBookings || "0", icon: ChartLineIcon },
-    { title: "Total Revenue", value: dashboardData.totalRevenue || "0", icon: CircleDollarSignIcon },
+    { title: "Total Revenue", value: currency + dashboardData.totalRevenue || "0", icon: CircleDollarSignIcon },
     { title: "Active Shows", value: dashboardData.activeShows.length || "0", icon: PlayCircleIcon },
     { title: "Total Users", value: dashboardData.totalUser || "0", icon: UsersIcon },
   ]
@@ -41,6 +41,7 @@ const Dashboard = () => {
 
       <div className="relative flex flex-wrap gap-4 mt-6">
         <BlurCircle top="-100px" left="0" />
+
         <div className="flex flex-wrap gap-4 w-full">
           {dashboardCards.map((card, index) => (
             <div key={index} className="flex items-center justify-between px-4 py-3 bg-primary/10
@@ -51,11 +52,24 @@ const Dashboard = () => {
               </div>
             </div>
           ))}
-
         </div>
 
+      </div>
+
+      <p className="mt-10 text-lg font-medium">Active Shows</p>
+
+      <div className="relative flex flex-wrap gap-6 mt-4 max-w-5xl">
+        <BlurCircle top="100px" left="-10%" />
+        {dashboardData.activeShows.map((show) => (
+          <div key={show._id} className="w-55 rounded-lg overflow-hidden h-full pb-3 bg-primary/10 border border-primary/20
+          hover:-translate-y-1 transition duration-300">
+            <img src={show.movie.poster_path} alt='' className="h-60 w-full object-cover" />
+            <p className="font-medium p-2 truncate">{show.movie.title}</p>
+          </div>
+        ))}
 
       </div>
+
 
     </>
   ) : <Loading />
