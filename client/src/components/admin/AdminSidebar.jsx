7a82af683a2183a17bf5b9b06fd8016ee7e30ef3
sujan2000 <meena.sujan14@gmail.com>
2025-12-02 14:44:14 +0530

    admin profile picture icon added

--- client/src/components/admin/AdminSidebar.jsx
+++ client/src/components/admin/AdminSidebar.jsx
@@ -1,5 +1,7 @@
 import { LayoutDashboardIcon, ListCollapseIcon, ListIcon, PlusSquareIcon } from 'lucide-react'
 import React from 'react'
+import { assets } from '../../assets/assets'
+
 
 const AdminSidebar = () => {
 
@@ -13,13 +15,18 @@ const AdminSidebar = () => {
         { name: 'Dashboard', path: '/admin', icon: LayoutDashboardIcon },
         { name: 'Add Shows', path: '/admin/add-shows', icon: PlusSquareIcon },
         { name: 'List Shows', path: '/admin/list-show', icon: ListIcon },
-        { name: 'List Bookings', path: '/admin/list-bookings', icon: ListCollapseIcon }
+        { name: 'List Bookings', path: '/admin/list-bookings', icon: ListCollapseIcon },
     ]
 
 
     return (
-        <div>Sidebar</div>
+
+        <div className="h-[calc(100vh-64px)] md:flex flex-col items-center pt-8 max-w-13 md:max-w-60 w-full
+        border-r border-gray-300/30 text-sm">
+            <img className="h-9 md:h-14 w-9 md:w-14 rounded-full mx-auto" src={user.imageUrl} alt="sidebar" />
+        </div>
     )
+
 }
 
 export default AdminSidebar
