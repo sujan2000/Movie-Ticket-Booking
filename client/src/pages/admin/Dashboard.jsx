5febcb6a7cb2a3ab7c6a5cc9c13d9525a0980d2b
sujan2000 <meena.sujan14@gmail.com>
2025-12-03 01:13:14 +0530

    dashboard complete

--- client/src/pages/admin/Dashboard.jsx
+++ client/src/pages/admin/Dashboard.jsx
@@ -44,6 +44,7 @@ const Dashboard = () => {
         <BlurCircle top="-100px" left="0" />
 
         <div className="flex flex-wrap gap-4 w-full">
+
           {dashboardCards.map((card, index) => (
             <div key={index} className="flex items-center justify-between px-4 py-3 bg-primary/10
             border border-primary/20 rounded-md max-w-50 w-full">
@@ -53,6 +54,7 @@ const Dashboard = () => {
               </div>
             </div>
           ))}
+          
         </div>
 
       </div>
@@ -61,11 +63,14 @@ const Dashboard = () => {
 
       <div className="relative flex flex-wrap gap-6 mt-4 max-w-5xl">
         <BlurCircle top="100px" left="-10%" />
+
         {dashboardData.activeShows.map((show) => (
           <div key={show._id} className="w-55 rounded-lg overflow-hidden h-full pb-3 bg-primary/10 border border-primary/20
           hover:-translate-y-1 transition duration-300">
+
             <img src={show.movie.poster_path} alt='' className="h-60 w-full object-cover" />
             <p className="font-medium p-2 truncate">{show.movie.title}</p>
+
             <div className="flex items-center justify-between px-2">
               <p className="text-lg font-medium">{currency} {show.showPrice}</p>
               <p className="flex items-center gap-1 text-sm text-gray-400 mt-1 pr-1">
@@ -73,9 +78,13 @@ const Dashboard = () => {
                 {show.movie.vote_average.toFixed(1)}
               </p>
             </div>
-            <p className="px-2 pt-2 text-sm text-gray-500">{dateFormat(show.showDateTime)}</p>
+
+            <p className="px-2 pt-2 text-sm text-gray-500">
+              {dateFormat(show.showDateTime)}
+            </p>
           </div>
         ))}
+
 
       </div>
 
