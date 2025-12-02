82593e86ce6a70481a29fdaf32b67ce8384f5ecc
sujan2000 <meena.sujan14@gmail.com>
2025-12-02 15:23:57 +0530

    requirments setup for dashboard

--- client/src/pages/admin/Dashboard.jsx
+++ client/src/pages/admin/Dashboard.jsx
@@ -1,8 +1,40 @@
-import React from 'react'
+import { ChartLineIcon, CircleDollarSignIcon, PlayCircleIcon, UsersIcon } from 'lucide-react';
+import React, { useEffect, useState } from 'react'
+import { dummyDashboardData } from '../../assets/assets';
 
 const Dashboard = () => {
+
+  const currency = import.meta.env.VITE_CURRENCY
+
+  const [dashboardData, setDashboardData] = useState({
+    totalBookings: 0,
+    totalRevenue: 0,
+    activeShows: [],
+    totalUser: 0
+  });
+
+  const [loading, setLoading] = useState(true)
+
+  const dashboardCards = [
+    { title: "Total Bookings", value: dashboardData.totalBookings || "0", icon: ChartLineIcon },
+    { title: "Total Revenue", value: dashboardData.totalRevenue || "0", icon: CircleDollarSignIcon },
+    { title: "Active Shows", value: dashboardData.activeShows.length || "0", icon: PlayCircleIcon },
+    { title: "Total Users", value: dashboardData.totalUser || "0", icon: UsersIcon },
+  ]
+
+  const fetchDashBoardData = async () => {
+    setDashboardData(dummyDashboardData)
+    setLoading(fasle)
+  }
+
+  useEffect(() => {
+    fetchDashBoardData()
+  }, [])
+
   return (
-    <div>Dashboard</div>
+    <div>
+
+    </div>
   )
 }
 
