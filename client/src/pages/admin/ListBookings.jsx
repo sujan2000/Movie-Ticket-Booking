5990ee478e25d9964d560aa3187089f7d361d3a3
sujan2000 <meena.sujan14@gmail.com>
2025-12-03 01:59:22 +0530

    data fetch

--- client/src/pages/admin/ListBookings.jsx
+++ client/src/pages/admin/ListBookings.jsx
@@ -1,8 +1,26 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { dummyBookingData } from '../../assets/assets'
 
 const ListBookings = () => {
+
+  const currency = import.meta.env.VITE_CURRENCY
+
+  const [bookings, setBookings] = useState([])
+  const [isLoading, setIsLoading] = useState(true)
+
+  const getAllBookings = async () => {
+    setBookings(dummyBookingData)
+    setIsLoading(false)
+  }
+
+  useEffect(() => {
+    getAllBookings()
+  }, [])
+
   return (
-    <div>ListBookings</div>
+    <div>
+
+    </div>
   )
 }
 
