5636ae785b47b509b54dd45b6874b719bc0e0dcb
sujan2000 <meena.sujan14@gmail.com>
2025-12-03 02:14:46 +0530

    table complete

--- client/src/pages/admin/ListBookings.jsx
+++ client/src/pages/admin/ListBookings.jsx
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from 'react'
 import { dummyBookingData } from '../../assets/assets'
 import Loading from '../../components/Loading'
 import Title from '../../components/admin/Title'
+import { dateFormat } from '../../lib/dateFormat'
 
 const ListBookings = () => {
 
@@ -35,7 +36,17 @@ const ListBookings = () => {
               <th className="p-2 font-medium">Amount</th>
             </tr>
           </thead>
-
+          <tbody className='text-sm font-light'>
+            {bookings.map((item, index) => (
+              <tr key={index} className="border-b border-primary/20 bg-primary/5 even:bg-primary/10">
+                <td className="p-2 min-w-45 pl-5">{item.user.name}</td>
+                <td className="p-2">{item.show.movie.title}</td>
+                <td className="p-2">{dateFormat(item.show.showDateTime)}</td>
+                <td className="p-2">{Object.keys(item.bookedSeats).map(seat => item.bookedSeats[seat]).join(", ")}</td>
+                <td className="p-2">{currency} {item.amount}</td>
+              </tr>
+            ))}
+          </tbody>
         </table>
 
       </div>
