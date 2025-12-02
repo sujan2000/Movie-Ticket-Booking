4e8c9f730eb0b00697126a1ed37d3b1990a98ffb
sujan2000 <meena.sujan14@gmail.com>
2025-12-03 02:07:07 +0530

    list booking table

--- client/src/pages/admin/ListBookings.jsx
+++ client/src/pages/admin/ListBookings.jsx
@@ -1,5 +1,7 @@
 import React, { useEffect, useState } from 'react'
 import { dummyBookingData } from '../../assets/assets'
+import Loading from '../../components/Loading'
+import Title from '../../components/admin/Title'
 
 const ListBookings = () => {
 
@@ -17,10 +19,30 @@ const ListBookings = () => {
     getAllBookings()
   }, [])
 
-  return (
-    <div>
+  return !isLoading ? (
 
-    </div>
+    <>
+      <Title text1="List" text2="Bookings" />
+
+      <div className='max-w-4xl mt-6 overflow-x-auto'>
+        <table className="w-full border-collapse rounded-md overflow-hidden text-nowrap">
+          <thead>
+            <tr className="bg-primary/20 text-left text-white">
+              <th className="p-2 font-medium pl-5">User Name</th>
+              <th className="p-2 font-medium">Movie Name</th>
+              <th className="p-2 font-medium">Show Time</th>
+              <th className="p-2 font-medium">Seats</th>
+              <th className="p-2 font-medium">Amount</th>
+            </tr>
+          </thead>
+
+        </table>
+
+      </div>
+    </>
+
+  ) : (
+    <Loading />
   )
 }
 
