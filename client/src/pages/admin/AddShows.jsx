f8a0091a6bf1339d62fc7e4ef89f8759aaf26759
sujan2000 <meena.sujan14@gmail.com>
2025-12-03 02:36:00 +0530

    add shows card add

--- client/src/pages/admin/AddShows.jsx
+++ client/src/pages/admin/AddShows.jsx
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from 'react'
 import { dummyShowsData } from '../../assets/assets'
 import Loading from '../../components/Loading'
 import Title from '../../components/admin/Title'
+import { StarIcon } from 'lucide-react'
 
 const AddShows = () => {
 
@@ -24,7 +25,25 @@ const AddShows = () => {
   return nowPlayingMovies.length > 0 ? (
     <>
       <Title text1="Add" text2="Shows" />
+      <p className="mt-10 text-lg font-medium">Now Playing Movies</p>
 
+      <div className="overflow-x-auto pb-4">
+        <div className="group flex flex-wrap gap-4 mt-4 w-max">
+          {nowPlayingMovies.map((movie) => (
+            <div key={movie.id} className={`relative max-w-40 cursor-pointer group-hover:not-hover:opacity-40
+                hover:-translate-y-1 transition duration-300`}>
+              <div className='relative rounded-lg overflow-hidden'>
+                <img src={movie.poster_path} alt="" className='w-full object-cover brightness-90' />
+                <StarIcon className='w-4 h-4 text-primary fill-primary' />
+                {movie.vote_average.toFixed(1)}
+              </div>
+
+            </div>
+
+          ))}
+
+        </div>
+      </div>
     </>
   ) : (
     <Loading />
