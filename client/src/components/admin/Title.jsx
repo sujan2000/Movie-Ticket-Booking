29662c3e14782bb768fa93ff8374398111723c16
sujan2000 <meena.sujan14@gmail.com>
2025-12-02 15:57:17 +0530

    {  glitch solve

--- client/src/components/admin/Title.jsx
+++ client/src/components/admin/Title.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 
-const Title = (text1, text2) => {
+const Title = ({ text1, text2 }) => {
     return (
         <h1 className="font-medium text-2xl">
             {text1} <span className="underline text-primary">{text2}</span>
