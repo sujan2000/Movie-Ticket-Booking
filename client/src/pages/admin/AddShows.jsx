e368e1865aa56d9fa5f1f636936185fb35766f1e
sujan2000 <meena.sujan14@gmail.com>
2025-12-03 02:26:42 +0530

    data fetched

--- client/src/pages/admin/AddShows.jsx
+++ client/src/pages/admin/AddShows.jsx
@@ -1,8 +1,33 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { dummyShowsData } from '../../assets/assets'
+import Loading from '../../components/Loading'
+import Title from '../../components/admin/Title'
 
 const AddShows = () => {
-  return (
-    <div>AddShows</div>
+
+  const currency = import.meta.env.VITE_CURRENCY
+  const [nowPlayingMovies, setNowPlayingMovies] = useState([])
+  const [dateTimeSelection, setDateTimeSelection] = useState({})
+  const [dateTimeInput, setDateTimeInput] = useState("")
+  const [showPrice, setShowPrice] = useState('')
+  const [selectedMovie, setSelectedMovie] = useState(null)
+
+
+  const fetchNowPlayingMovies = async () => {
+    setNowPlayingMovies(dummyShowsData);
+  }
+
+  useEffect(() => {
+    fetchNowPlayingMovies()
+  },)
+
+  return nowPlayingMovies.length > 0 ? (
+    <>
+      <Title text1="Add" text2="Shows" />
+
+    </>
+  ) : (
+    <Loading />
   )
 }
 
