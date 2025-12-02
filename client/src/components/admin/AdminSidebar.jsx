9de148a85ee1236a1f9e0b57f67b630aeadca0c8
sujan2000 <meena.sujan14@gmail.com>
2025-12-02 14:36:45 +0530

    navbar links add

--- client/src/components/admin/AdminSidebar.jsx
+++ client/src/components/admin/AdminSidebar.jsx
@@ -1,9 +1,25 @@
+import { LayoutDashboardIcon, ListCollapseIcon, ListIcon, PlusSquareIcon } from 'lucide-react'
 import React from 'react'
 
 const AdminSidebar = () => {
-  return (
-    <div>Sidebar</div>
-  )
+
+    const user = {
+        firstName: 'Admin',
+        lastName: 'User',
+        imageUrl: assets.profile,
+    }
+
+    const adminNavlinks = [
+        { name: 'Dashboard', path: '/admin', icon: LayoutDashboardIcon },
+        { name: 'Add Shows', path: '/admin/add-shows', icon: PlusSquareIcon },
+        { name: 'List Shows', path: '/admin/list-show', icon: ListIcon },
+        { name: 'List Bookings', path: '/admin/list-bookings', icon: ListCollapseIcon }
+    ]
+
+
+    return (
+        <div>Sidebar</div>
+    )
 }
 
 export default AdminSidebar
