01ed460161094b49f11cc3c8710bbba420b0afb7
sujan2000 <meena.sujan14@gmail.com>
2025-12-02 16:04:40 +0530

    dashboard cards added

--- client/src/pages/admin/Dashboard.jsx
+++ client/src/pages/admin/Dashboard.jsx
@@ -1,6 +1,9 @@
 import { ChartLineIcon, CircleDollarSignIcon, PlayCircleIcon, UsersIcon } from 'lucide-react';
 import React, { useEffect, useState } from 'react'
 import { dummyDashboardData } from '../../assets/assets';
+import Loading from '../../components/Loading';
+import Title from '../../components/admin/Title';
+import BlurCircle from '../../components/BlurCircle';
 
 const Dashboard = () => {
 
@@ -24,18 +27,38 @@ const Dashboard = () => {
 
   const fetchDashBoardData = async () => {
     setDashboardData(dummyDashboardData)
-    setLoading(fasle)
+    setLoading(false)
   }
 
   useEffect(() => {
     fetchDashBoardData()
   }, [])
 
-  return (
-    <div>
+  return !loading ? (
+    <>
 
-    </div>
-  )
+      <Title text1="Admin" text2="Dashboard" />
+
+      <div className="relative flex flex-wrap gap-4 mt-6">
+        <BlurCircle top="-100px" left="0" />
+        <div className="flex flex-wrap gap-4 w-full">
+          {dashboardCards.map((card, index) => (
+            <div key={index} className="flex items-center justify-between px-4 py-3 bg-primary/10
+            border border-primary/20 rounded-md max-w-50 w-full">
+              <div>
+                <h1 className="text-sm">{card.title}</h1>
+                <p className="text-xl font-medium mt-1">{card.value}</p>
+              </div>
+            </div>
+          ))}
+
+        </div>
+
+
+      </div>
+
+    </>
+  ) : <Loading />
 }
 
 export default Dashboard
