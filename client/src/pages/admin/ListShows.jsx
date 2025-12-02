1343c3089fcbffc237f795a50de1336e7abcbab1
sujan2000 <meena.sujan14@gmail.com>
2025-12-03 01:45:54 +0530

    table head added

--- client/src/pages/admin/ListShows.jsx
+++ client/src/pages/admin/ListShows.jsx
@@ -1,5 +1,7 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import { dummyShowsData } from '../../assets/assets'
+import Loading from '../../components/Loading'
+import Title from '../../components/admin/Title'
 
 const ListShows = () => {
 
@@ -28,11 +30,33 @@ const ListShows = () => {
     }
   }
 
-  return (
-    <div>
+  useEffect(() => {
+    getAllShows()
+  }, [])
 
-    </div>
-  )
+  return !loading ? (
+    <>
+      <Title text1="List" text2="Shows" />
+
+      <div className="max-w-4xl mt-6 overflow-x-auto">
+        <table className="w-full border-collapse rounded-md overflow-hidden text-nowrap">
+          <thead>
+            <tr className='bg-primary/20 text-left text-white'>
+              <th className='p-2 font-medium pl-5'>Movie Name</th>
+              <th className='p-2 font-medium'>Show Time</th>
+              <th className='p-2 font-medium'>Total Bookings</th>
+              <th className='p-2 font-medium'>Earnings</th>
+            </tr>
+          </thead>
+          <tbody className='text-sm font-light'>
+              {}
+          </tbody>
+        </table>
+      </div>
+    </>
+
+  ) : <Loading />
+
 }
 
 export default ListShows
