16627663a56e699e90e91bde4db97cac976af6a6
sujan2000 <meena.sujan14@gmail.com>
2025-12-03 22:09:13 +0530

    date time and hadleDateTimeAdd func add

--- client/src/pages/admin/AddShows.jsx
+++ client/src/pages/admin/AddShows.jsx
@@ -19,6 +19,19 @@ const AddShows = () => {
     setNowPlayingMovies(dummyShowsData);
   }
 
+  const handleDateTimeAdd = () => {
+    if (!dateTimeInput) return;
+    const [date, time] = dateTimeInput.split('T');
+    if (!date || !time) return;
+
+    setDateTimeSelection((prev) => {
+      const times = prev[date] || [];
+      if (!times.includes(time)) {
+        return { ...prev, [date]: [...time, time] }
+      }
+    })
+  }
+
   useEffect(() => {
     fetchNowPlayingMovies()
   },)
@@ -66,7 +79,17 @@ const AddShows = () => {
           <input min={0} type="number" value={showPrice} className='outline-none'
             onChange={(e) => setShowPrice(e.target.value)} placeholder='Enter show price' />
         </div>
+      </div>
 
+      {/* date & time */}
+      <div className='mt-6'>
+        <label className='block text-sm font-medium mb-2'>Select Date abd Time</label>
+        <div className="inline-flex gap-5 border border-gray-600 p-1 pl-3 rounded-lg">
+          <input type="datetime-local" value={dateTimeInput} onChange={(e) => setDateTimeInput(e.target.value)}
+            className='outline-none rounded-md' />
+          <button onClick={handleDateTimeAdd} className='bg-primary/80 text-white px-3 py-2 rounded-lg
+            hover:bg-primary cursor-pointer'>Add Time</button>
+        </div>
       </div>
 
     </>
