6981016e90681a88a130ebe0adf98fce98d11647
sujan2000 <meena.sujan14@gmail.com>
2025-12-05 09:07:36 +0530

    clerk express for data added

--- server/server.js
+++ server/server.js
@@ -2,6 +2,7 @@ import express from 'express';
 import cors from 'cors';
 import 'dotenv/config'
 import connectDB from './configs/db.js';
+import { clerkMiddleware } from '@clerk/express'
 
 const app = express();
 
@@ -12,6 +13,7 @@ await connectDB()
 //middleware
 app.use(express.json())
 app.use(cors())
+app.use(clerkMiddleware())
 
 
 //api routes
