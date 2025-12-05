29754189c883dea52bb04e61642dacf5eadeea8e
sujan2000 <meena.sujan14@gmail.com>
2025-12-05 19:08:33 +0530

    user creation func add

--- server/inngest/index.js
+++ server/inngest/index.js
@@ -1,11 +1,27 @@
 import { Inngest } from "inngest";
+import User from "../models/User";
 
 
 // Create a client to send and receive events
 export const inngest = new Inngest({ id: "movie-ticket-booking" });
 
+//inngest function to save user data to a database
 
+const syncUserCreation = inngest.createFunction(
+    { id: 'sync-user-from-clerk' },
+    { event: 'clerk/user.created' },
+    async ({ event }) => {
+        const { id, first_name, last_name, email_addresses, image_url } = event.data
+        const userData = {
+            _id: id,
+            email: email_addresses[0].email_address,
+            name: first_name + ' ' + last_name,
+            iamge: image_url
+        }
+        await User.create(userData)
+    }
+)
 
 
 // Create an empty array where we'll export future Inngest functions
-export const functions = [];
+export const functions = [syncUserCreation];
