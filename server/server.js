2b29ce011fd411c132622521f51b73acca215af6
sujan2000 <meena.sujan14@gmail.com>
2025-12-05 08:53:17 +0530

    mongoDB connect to server

---
+++ server/server.js
@@ -0,0 +1,22 @@
+import express from 'express';
+import cors from 'cors';
+import 'dotenv/config'
+import connectDB from './configs/db.js';
+
+const app = express();
+
+const port = 3000;
+
+await connectDB()
+
+//middleware
+app.use(express.json())
+app.use(cors())
+
+
+//api routes
+app.get('/', (req, res) =>
+    res.send('Server is Live!')
+)
+
+app.listen(port, () => console.log(`Server listening at http://localhost:${port}`))
