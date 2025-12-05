bbcd756f4a9b0375ab33ff24841230789e89ef07
sujan2000 <meena.sujan14@gmail.com>
2025-12-05 09:46:41 +0530

    add

---
+++ server/inngest/index.js
@@ -0,0 +1,11 @@
+import { Inngest } from "inngest";
+
+
+// Create a client to send and receive events
+export const inngest = new Inngest({ id: "movie-ticket-booking" });
+
+
+
+
+// Create an empty array where we'll export future Inngest functions
+export const functions = [];
--- server/server.js
+++ server/server.js
@@ -3,6 +3,8 @@ import cors from 'cors';
 import 'dotenv/config'
 import connectDB from './configs/db.js';
 import { clerkMiddleware } from '@clerk/express'
+import { serve } from "inngest/express";
+import { inngest, functions } from "./inngest/index.js"
 
 const app = express();
 
@@ -20,5 +22,7 @@ app.use(clerkMiddleware())
 app.get('/', (req, res) =>
     res.send('Server is Live!')
 )
+
+app.use('/api/inngest', serve({ client: inngest, functions }))
 
 app.listen(port, () => console.log(`Server listening at http://localhost:${port}`))
