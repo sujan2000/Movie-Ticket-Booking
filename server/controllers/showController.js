0e1ddceadfd299a84e963b551019362615c465e7
sujan2000 <meena.sujan14@gmail.com>
2025-12-07 23:53:02 +0530

    fatch data form api endpoint

--- server/controllers/showController.js
+++ server/controllers/showController.js
@@ -1,11 +1,16 @@
 import axios from "axios"
 
-export const getNowPlayingMovies = async (res, req) => {
+export const getNowPlayingMovies = async (req, res) => {
     try {
-        await axios.get('https://api.themoviedb.org/3/movie/now_playing', {
+        const { data } = await axios.get('https://api.themoviedb.org/3/movie/now_playing', {
             headers: { Authorization: `Bearer ${process.env.TMDB_API_KEY}` }
         })
+
+        const movies = data.results;
+        res.json({ success: true, movies: movies })
     } catch (error) {
+        console.error(error);
+        res.json({ success: false, message: error.message })
 
     }
 }
