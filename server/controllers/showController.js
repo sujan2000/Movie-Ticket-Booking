a4bba0cc0352539140b506f267da9b199bb6e48a
sujan2000 <meena.sujan14@gmail.com>
2025-12-08 00:34:50 +0530

    api get  method add

--- server/controllers/showController.js
+++ server/controllers/showController.js
@@ -1,5 +1,8 @@
 import axios from "axios"
+import Movie from "../models/Movie.js";
 
+
+//API to get now playing movies from TMDB API
 export const getNowPlayingMovies = async (req, res) => {
     try {
         const { data } = await axios.get('https://api.themoviedb.org/3/movie/now_playing', {
@@ -8,6 +11,34 @@ export const getNowPlayingMovies = async (req, res) => {
 
         const movies = data.results;
         res.json({ success: true, movies: movies })
+    } catch (error) {
+        console.error(error);
+        res.json({ success: false, message: error.message })
+
+    }
+}
+
+//API to add now show to the database
+export const addShow = async (req, res) => {
+    try {
+        const { movieId, showsInput, showPrice } = req.body
+        let movie = await Movie.findById(movieId)
+
+        if (!movie) {
+            const [movieDetailsResponse, movieCreditsResponse] = await Promise.all([
+                axios.get(`https://api.themoviedb.org/3/movie/${movieId}`, {
+                    headers: { Authorization: `Bearer ${process.env.TMDB_API_KEY}` }
+                }),
+
+                axios.get(`https://api.themoviedb.org/3/movie/${movieId}/credits`, {
+                    headers: { Authorization: `Bearer ${process.env.TMDB_API_KEY}` }
+                })
+
+            ]);
+
+            const movieApiData = movieDetailsResponse.data
+        }
+
     } catch (error) {
         console.error(error);
         res.json({ success: false, message: error.message })
