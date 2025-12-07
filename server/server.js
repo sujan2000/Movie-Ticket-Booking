3dfc4841ec3e48477a04b9f920fc3eec9dc3ccfb
sujan2000 <meena.sujan14@gmail.com>
2025-12-08 00:06:19 +0530

    api  show router added

--- server/server.js
+++ server/server.js
@@ -5,6 +5,7 @@ import connectDB from './configs/db.js';
 import { clerkMiddleware } from '@clerk/express'
 import { serve } from "inngest/express";
 import { inngest, functions } from "./inngest/index.js"
+import showRouter from './routes/showRoutes.js';
 
 const app = express();
 
@@ -18,11 +19,11 @@ app.use(cors())
 app.use(clerkMiddleware())
 
 
-//api routes
-app.get('/', (req, res) =>
-    res.send('Server is Live!')
-)
-
+//Api routes
+app.get('/', (req, res) => res.send('Server is Live!'))
 app.use('/api/inngest', serve({ client: inngest, functions }))
+app.use('/api/show', showRouter)
+
+
 
 app.listen(port, () => console.log(`Server listening at http://localhost:${port}`))
