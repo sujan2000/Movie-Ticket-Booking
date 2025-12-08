2808c923eb5a6e427b9b44a4bb8962222e391cad
sujan2000 <meena.sujan14@gmail.com>
2025-12-09 01:17:35 +0530

    admin router added

--- server/server.js
+++ server/server.js
@@ -7,6 +7,7 @@ import { serve } from "inngest/express";
 import { inngest, functions } from "./inngest/index.js"
 import showRouter from './routes/showRoutes.js';
 import bookingRouter from './routes/bookingRoutes.js';
+import adminRouter from './routes/adminRoutes.js';
 
 const app = express();
 
@@ -25,7 +26,7 @@ app.get('/', (req, res) => res.send('Server is Live!'))
 app.use('/api/inngest', serve({ client: inngest, functions }))
 app.use('/api/show', showRouter)
 app.use('/api/booking', bookingRouter)
-
+app.use('/api/admin', adminRouter)
 
 
 app.listen(port, () => console.log(`Server listening at http://localhost:${port}`))
