6795f271b72e8e7711fc880d451bfb87be56beb9
sujan2000 <meena.sujan14@gmail.com>
2025-12-09 00:54:05 +0530

    api to add dashboard data to  database created

---
+++ server/controllers/adminController.js
@@ -0,0 +1,31 @@
+import Booking from "../models/Booking.js"
+import Show from "../models/Show.js";
+import User from "../models/User.js";
+
+
+
+//API to check if user is admin
+export const isAdmin = async (req, res) => {
+    res.json({ success: true, isAdmin: true })
+}
+
+//API to get dashboard data
+export const getDashboardData = async (req, res) => {
+    try {
+
+        const bookings = await Booking.find({ isPaid: true });
+        const activeShows = await Show.find({ showDateTime: { $gte: new Date() } }).populate('movie')
+        const totalUser = await User.countDocuments();
+
+        const dashboardData = {
+            totalBookings: bookings.length,
+            totalRevenue: bookings.reduce((acc, booking) => acc + booking.amount, 0),
+            activeShows,
+            totalUser
+        }
+        res.json({ success: true, dashboardData })
+    } catch (error) {
+        console.error(error)
+        res.json({ success: false, message: error.message })
+    }
+}
