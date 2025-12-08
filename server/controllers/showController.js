fbd8dcd4a7b0b1adada08338677dd79498fd7239
sujan2000 <meena.sujan14@gmail.com>
2025-12-08 19:07:54 +0530

    api to single show add

--- server/controllers/showController.js
+++ server/controllers/showController.js
@@ -121,6 +121,30 @@ export const getShows = async () => {
         console.error(error);
         res.json({ success: false, message: error.message })
 
+    }
+}
+
+//API to get single show from the database
+
+export const getShow = async (req, res) => {
+    try {
+        const { movieId } = req.params;
+        const shows = await Show.find({ movie: movieId, showDateTime: { $gte: new Date() } })
+        const movie = await Movie.findById(movieId)
+        const dateTime = {};
+
+        shows.forEach((show) => {
+            const date = show.showDateTime.toISOString().split("T")[0]
+            if (!dateTime[date]) {
+                dateTime[date] = []
+            }
+            dateTime[date].push({ time: show.showDateTime, showId: show._id })
+        })
+
+        res.json({ success: true, movie, dateTime })
+    } catch (error) {
+        console.error(error);
+        res.json({ success: false, message: error.message })
 
     }
 }
