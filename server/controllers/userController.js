3a724b31a1d0c2e0809d0c0f8ee5be30dee0764b
sujan2000 <meena.sujan14@gmail.com>
2025-12-09 01:51:04 +0530

    favorite show  as metadata create

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,3 +1,4 @@
+import { clerkClient } from "@clerk/express";
 import Booking from "../models/Booking.js";
 
 
@@ -16,5 +17,30 @@ export const getUserBookings = async (req, res) => {
     } catch (error) {
         console.error(error.message)
         res.json({ success: false, message: error.message });
+    }
+}
+
+//API controller function to add favorite movie in clerk user metadata
+
+export const addFavorite = async (req, res) => {
+    try {
+        const { movieId } = req.body;
+        const userId = req.auth().userId;
+
+        const user = await clerkClient.users.getUser(userId)
+        if (!user.privateMetadata.favorites) {
+            user.privateMetadata.favorites = []
+        }
+
+        if (!user.privateMetadata.favorites.includes(movieId)) {
+            user.privateMetadata.favorites.push(movieId)
+        }
+
+        await clerkClient.users.updateUserMetadata(userId, { privateMetadata: user.privateMetadata })
+
+        res.json({ success: true, message: "Favorite added successfully" })
+    } catch (error) {
+        console.error(error.message);
+        res.json({ success: false, message: error.message })
     }
 }
