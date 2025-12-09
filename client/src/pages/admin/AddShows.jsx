e55c6b3a3cc136d44f87d947b249a1c3d6c89d90
sujan2000 <meena.sujan14@gmail.com>
2025-12-09 18:52:34 +0530

    images to add  shows added

--- client/src/pages/admin/AddShows.jsx
+++ client/src/pages/admin/AddShows.jsx
@@ -4,20 +4,37 @@ import Loading from '../../components/Loading'
 import Title from '../../components/admin/Title'
 import { CheckIcon, DeleteIcon, StarIcon } from 'lucide-react'
 import { kConverter } from '../../lib/kConverter'
+import { useAppContext } from '../../context/AppContext'
 
 
 const AddShows = () => {
 
+  const { axios, getToken, user, image_base_url } = useAppContext()
+
   const currency = import.meta.env.VITE_CURRENCY
   const [nowPlayingMovies, setNowPlayingMovies] = useState([])
+  const [selectedMovie, setSelectedMovie] = useState(null)
   const [dateTimeSelection, setDateTimeSelection] = useState({})
   const [dateTimeInput, setDateTimeInput] = useState("")
   const [showPrice, setShowPrice] = useState('')
-  const [selectedMovie, setSelectedMovie] = useState(null)
 
 
   const fetchNowPlayingMovies = async () => {
-    setNowPlayingMovies(dummyShowsData);
+
+    try {
+
+      const { data } = await axios.get('/api/show/now-playing', {
+        headers:
+          { Authorization: `Bearer ${await getToken()}` }
+      })
+      if (data.success) {
+        setNowPlayingMovies(data.movies)
+      }
+
+    } catch (error) {
+      console.error('Error fetching movies:', error)
+    }
+
   }
 
   const handleDateTimeAdd = () => {
@@ -49,8 +66,10 @@ const AddShows = () => {
   }
 
   useEffect(() => {
-    fetchNowPlayingMovies()
-  },)
+    if (user) {
+      fetchNowPlayingMovies()
+    }
+  }, [user])
 
   return nowPlayingMovies.length > 0 ? (
     <>
@@ -63,7 +82,7 @@ const AddShows = () => {
             <div key={movie.id} className={`relative max-w-40 cursor-pointer group-hover:not-hover:opacity-40
                 hover:-translate-y-1 transition duration-300`} onClick={() => setSelectedMovie(movie._id)}>
               <div className='relative rounded-lg overflow-hidden'>
-                <img src={movie.poster_path} alt="" className='w-full object-cover brightness-90' />
+                <img src={image_base_url + movie.poster_path} alt="" className='w-full object-cover brightness-90' />
                 <div className='text-sm flex items-center justify-between p-2 bg-black/70
                    w-full absolute bottom-0 left-0'>
                   <p className="flex items-center gap-1 text-gray-400">
