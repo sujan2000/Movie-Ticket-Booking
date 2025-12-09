f1323cdd65dd2f441f41cb8740e9b63405a61267
sujan2000 <meena.sujan14@gmail.com>
2025-12-09 19:13:08 +0530

    button add show now working

--- client/src/pages/admin/AddShows.jsx
+++ client/src/pages/admin/AddShows.jsx
@@ -5,6 +5,7 @@ import Title from '../../components/admin/Title'
 import { CheckIcon, DeleteIcon, StarIcon } from 'lucide-react'
 import { kConverter } from '../../lib/kConverter'
 import { useAppContext } from '../../context/AppContext'
+import toast from 'react-hot-toast'
 
 
 const AddShows = () => {
@@ -17,7 +18,7 @@ const AddShows = () => {
   const [dateTimeSelection, setDateTimeSelection] = useState({})
   const [dateTimeInput, setDateTimeInput] = useState("")
   const [showPrice, setShowPrice] = useState('')
-
+  const [addingShow, setAddingShow] = useState(false)
 
   const fetchNowPlayingMovies = async () => {
 
@@ -63,6 +64,45 @@ const AddShows = () => {
         [date]: filteredTimes,
       }
     })
+  }
+
+  const handleSubmit = async () => {
+    try {
+      setAddingShow(true)
+
+      if (!selectedMovie || Object.keys(dateTimeSelection).length === 0 || !showPrice) {
+        return toast('Missing required fields')
+      }
+
+      const showsInput = Object.entries(dateTimeSelection).map(
+        ([date, time]) => ({ date, time })
+      );
+
+      const payload = {
+        movieId: selectedMovie,
+        showsInput,
+        showPrice: Number(showPrice)
+      }
+      const { data } = await axios.post('/api/show/add', payload, {
+        headers: {
+          Authorization: `Bearer ${await getToken()}`
+        }
+      })
+
+      if (data.seccess) {
+        toast.success(data.message)
+        setSelectedMovie(null)
+        setDateTimeSelection({})
+        setShowPrice("")
+      } else {
+        toast.error(data.message)
+      }
+    } catch (error) {
+      console.error("Submission error:", error)
+      toast.error('An error occurred. Please try again.')
+    }
+
+    setAddingShow(false)
   }
 
   useEffect(() => {
@@ -151,7 +191,7 @@ const AddShows = () => {
         </div>
       )}
 
-      <button className='bg-primary text-white px-8 py-2 mt-6 rounded hover:bg-primary/90
+      <button onClick={handleSubmit} disabled={addingShow} className='bg-primary text-white px-8 py-2 mt-6 rounded hover:bg-primary/90
       transition-all cursor-pointer'>
         Add Show
       </button>
