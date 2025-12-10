5c2b9902e4a00c24d4335c04b5348380ef123b46
sujan2000 <meena.sujan14@gmail.com>
2025-12-11 01:29:50 +0530

    work fine

--- client/src/pages/MovieDetails.jsx
+++ client/src/pages/MovieDetails.jsx
@@ -99,7 +99,7 @@ const MovieDetails = () => {
         <div className="flex items-center gap-4 w-max px-4">
           {show.movie.casts.slice(0, 12).map((cast, index) => (
             <div key={cast.id || index} className="flex flex-col items-center text-center">
-              <img src={cast.profile_path} alt=""
+              <img src={image_base_url + cast.profile_path} alt=""
                 className="rounded-full h-20 md:h-20 aspect-square object-cover" />
               <p className="font-medium text-xs mt-3">{cast.name}</p>
             </div>
