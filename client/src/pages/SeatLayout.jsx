9b1d021c5b2258156fa9461fbd83a348e4e12dec
sujan2000 <meena.sujan14@gmail.com>
2025-12-12 20:50:21 +0530

    chnage

--- client/src/pages/SeatLayout.jsx
+++ client/src/pages/SeatLayout.jsx
@@ -93,8 +93,8 @@ const SeatLayout = () => {
         { headers: { Authorization: `Bearer ${await getToken()}` } })
 
       if (data.success) {
-        toast.success(data.message)
-        navigate('/my-bookings')
+         window.location.href = data.url;
+
       } else {
         toast.error(data.message)
       }
