abe18f3622792dfc43eebe9c1bcce1990ea9e937
sujan2000 <meena.sujan14@gmail.com>
2025-12-13 23:56:44 +0530

    change in inngest  serve

--- server/server.js
+++ server/server.js
@@ -9,12 +9,16 @@ import showRouter from './routes/showRoutes.js';
 import bookingRouter from './routes/bookingRoutes.js';
 import adminRouter from './routes/adminRoutes.js';
 import userRouter from './routes/userRoutes.js';
+import { stripeWebhooks } from './controllers/stripeWebhooks.js';
 
 const app = express();
 
 const port = 3000;
 
 await connectDB()
+
+//Stripe webhook route
+app.use('/api/stripe', express.raw({ type: 'application/json' }), stripeWebhooks)
 
 //middleware
 app.use(express.json())
@@ -24,7 +28,14 @@ app.use(clerkMiddleware())
 
 //Api routes
 app.get('/', (req, res) => res.send('Server is Live!'))
-app.use('/api/inngest', serve({ client: inngest, functions }))
+
+
+export default serve({
+  client: inngest,
+  functions,
+});
+
+// app.use('/api/inngest', serve({ client: inngest, functions }))
 app.use('/api/show', showRouter)
 app.use('/api/booking', bookingRouter)
 app.use('/api/admin', adminRouter)
