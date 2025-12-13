1e46bbb592db3a366a861932cc7ca00d86ae57ee
sujan2000 <meena.sujan14@gmail.com>
2025-12-14 00:09:12 +0530

    check

--- api/inngest.js
+++ api/inngest.js
@@ -1,6 +1,11 @@
 import { serve } from "inngest/express";
 import { inngest, functions } from "../server/inngest/index.js";
 
+console.log("🔥 INNGEST API FILE LOADED");
+console.log(
+    "🔥 FUNCTIONS:",
+    functions.map(f => f.id)
+);
 export default serve({
     client: inngest,
     functions,
