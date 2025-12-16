257a6d17f4eda08ece10793c0e27a344d70713ff
sujan2000 <meena.sujan14@gmail.com>
2025-12-16 15:23:20 +0530

    no change

--- client/src/components/admin/AdminNavbar.jsx
+++ client/src/components/admin/AdminNavbar.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
-import { assets } from '../../assets/assets'
+import { assets } from '../../assets/assets.js'
 
 const AdminNavbar = () => {
     return (
