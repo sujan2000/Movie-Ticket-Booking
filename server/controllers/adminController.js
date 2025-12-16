739f1ae6a9c26e84fbe50f27a6a5eb3f8ca6fb91
sujan2000 <meena.sujan14@gmail.com>
2025-12-16 15:03:19 +0530

    showdatetime sort to one correction

--- server/controllers/adminController.js
+++ server/controllers/adminController.js
@@ -37,7 +37,7 @@ export const getAllShows = async (req, res) => {
     try {
         const shows = await Show.find(
             { showDateTime: { $gte: new Date() } }
-        ).populate('movie').sort({ showDateTime })
+        ).populate('movie').sort({ showDateTime: 1 })
 
         res.json({ success: true, shows })
     } catch (error) {
